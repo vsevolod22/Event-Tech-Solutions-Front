b84f6ff83336b55f0b52913c3d0dab3869cff69f
Siverin Nikita Aleksandrovich <corasks@yandex.ru>
2024-03-23 02:01:57 +0300

    [+] added webpack [+] new app frontent for frontend-react [~] will be change version react 18 -> 17

--- event_tech/settings.py
+++ event_tech/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'event_calendar.apps.EventCalendarConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
--- event_tech/urls.py
+++ event_tech/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('event_calendar.urls'))
+    path('', include('frontend.urls')),
+    path('event_calendar/', include('event_calendar.urls'))
 ]
